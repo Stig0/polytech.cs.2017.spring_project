aaec034181ec4cb2da8e06023febd6d76ff7daa1
NikolaevDenisPolyt <den11nik@yandex.ru>
2017-05-01 14:35:47 +0300

    Вынесение части функций из main.c в functions.c,j,добавление комментариев

--- src/functions.c
+++ src/functions.c
@@ -1,83 +1,190 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
 int clear(int b[100][100]);
-void knopka(int size_of_matrix, int *game_mode, int *game_window, int *count_of_clean, int b[100][100], float bouncer_x, float bouncer_y) {
-	/*Далее,действие,при нажатии на кнопку Start:
-	Переход в режим многократных действий
-	*/
-	if ((bouncer_y > 0 && bouncer_y < 80) && (bouncer_x > 180 && bouncer_x < 330)) {
-		(*game_mode) = 10;
+void main_algorithm(int b[100][100], int a[100][100], int number_of_neighbors, int size_of_matrix,int *game_mode) {
+	int  i = 0, j = 0,x=0,y=0;
+	for (j = 1; j < size_of_matrix; j++) {
+		for (i = 1; i <size_of_matrix; i++) {
+			if (b[i - 1][j - 1] == 1) number_of_neighbors++;
+			if (b[i][j - 1] == 1) number_of_neighbors++;
+			if (b[i + 1][j - 1] == 1) number_of_neighbors++;
+			if (b[i - 1][j] == 1) number_of_neighbors++;
+			if (b[i + 1][j] == 1) number_of_neighbors++;
+			if (b[i - 1][j + 1] == 1) number_of_neighbors++;
+			if (b[i][j + 1] == 1) number_of_neighbors++;
+			if (b[i + 1][j + 1] == 1) number_of_neighbors++;
+			if (b[i][j] == 1)
+			{
+				if ((number_of_neighbors == 2) || (number_of_neighbors == 3))a[i][j] = 1;
+				else {
+					a[i][j] = 0;
+
+				}
+			}
+			if (b[i][j] == 0) {
+				if ((number_of_neighbors == 3)) {
+					a[i][j] = 1;
+
+				}
+				else a[i][j] = 0;
+			}
+			number_of_neighbors = 0;
+
+		}
 	}
-	/*Далее,действие,при нажатии на кнопку Step:
-	Переход в режим однократного действия
-	*/
-	else if (((bouncer_y > 0 && bouncer_y < 80) && (bouncer_x > 340 && bouncer_x < 490))) {
-		(*game_mode) = 1;
+	//Часть кода,отвечающая за автоотановку игры при совпадении текущей и предыдущей матриц
+	int d = 0;
+	for (y = 1; y < size_of_matrix; y++) {
+		for (x = 1; x < size_of_matrix; x++)
+		{
+			if (b[x][y] == a[x][y]) d++;
+
+		}
 	}
-	/*Далее,действие,при нажатии на кнопку Stop:
-	Остановка игрового процесса
-	*/
-	 else if ((bouncer_y> 0 && bouncer_y < 80) && (bouncer_x > 500 && bouncer_x < 670)) {
-		 (*game_mode)= 3;
-	 }
-	 /*Далее,действие,при нажатии на кнопку Clear:
-	 Обнуление матрицы b
-	 */
-	 else if ((bouncer_y > 0 && bouncer_y < 80) && (bouncer_x > 680 && bouncer_x < 840)) {
-		 clear(b);
-	 }
-	 /*Далее,действие,при нажатии на кнопку Menu:
-	 Открытие стартового окна
-	 */
-	 else if ((bouncer_y > 525 && bouncer_y < 600) && (bouncer_x > 800 && bouncer_x < 1000)) {
-		( *game_window) = 0;
-		(*count_of_clean) = 99;
-	 }
-	 /*Далее,деяствие,при нажатии на кнопку Planer :
-	 Пtрвоначальная очиства матрицы b ,далее выполнение функции b1,которая присваивает матрице b нужные значения
-	 */
-	 else if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 155 && bouncer_x < 260)) {
-		 clear(b);
-		 planer(b);
-	 }
-	 /*Далее,деяствие,при нажатии на кнопку Rellay :
-	 Пtрвоначальная очиства матрицы b ,далее выполнение функции b2,которая присваивает матрице b нужные значения
-	 */
-	 else if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 290 && bouncer_x <380)) {
-		 clear(b);
-		 rellay(b);
-	 }
-	 //Кнопка "Twin Bees":
-	 else if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 430 && bouncer_x < 570)) {
-		 clear(b);
-		 Twin_Bees(b);
-	 }
-	 /*
-	 Далее идет просерка попадания пользователем в игровые ячейки ,если координаты мыши удовлетворяют
-	 определенным условиям-пользователь попадает в игровую ячейку,связанную с матрицей b ТО:
-	 если элемент b был равен 0 ,то он становится равным 1
-	 если элемент b был равен 1 ,то он становится равным 0
-	 */
-	 else {
-		 int x, y;
-		 for (y = 1; y < size_of_matrix; y++) {
-			 if ((bouncer_y > 100 - 160 / size_of_matrix + (400/ size_of_matrix) * y) && (bouncer_y < 100 + 160 / size_of_matrix + (400 / size_of_matrix) * y))
-				 for (x = 1; x < size_of_matrix; x++)
-				 {
-					 if ((bouncer_x > 320 - 160 / size_of_matrix + (400 / size_of_matrix) * x) && (bouncer_x < 320 + 160 / size_of_matrix + (400 / size_of_matrix) * x)) {
-						 if (b[x][y] == 1) {
-							 b[x][y] = 0;
-						 }
-						 else if ((b[x][y] == 0)) {
-							 b[x][y] = 1;
+	if (d >size_of_matrix*size_of_matrix - 2 * size_of_matrix) { d = 0; (*game_mode) = 0; }
 
-						 }
-
-					 }
-
-				 }
-		 }
-	 }
-	
+	//Приравнивание текущей и предыдущей матриц 
+	for (y = 1; y < size_of_matrix; y++) {
+		for (x = 1; x < size_of_matrix; x++)
+		{
+			b[x][y] = a[x][y];
+			
+		}
+	}
 }
+void size_matrix(int *size_of_matrix, float bouncer_x, float bouncer_y) {
+	if ( (bouncer_y > 370 && bouncer_y < 405) && (bouncer_x > 640 && bouncer_x < 850)) {
+		(*size_of_matrix )= 10;
+
+
+	}
+	else if (  (bouncer_y > 405 && bouncer_y < 435) && (bouncer_x > 640 && bouncer_x < 850)) {
+		(*size_of_matrix) = 20;
+
+	}
+	else if (  (bouncer_y > 435 && bouncer_y < 465) && (bouncer_x > 640 && bouncer_x < 850)) {
+		(*size_of_matrix) = 30;
+
+	}
+	else if (  (bouncer_y > 465 && bouncer_y < 495) && (bouncer_x > 640 && bouncer_x < 850)) {
+		(*size_of_matrix) = 40;
+
+	}
+}
+void knopka(int size_of_matrix, int *Game_on, int *size_settings,int *game_mode, int *game_window, int *count_of_clean, int b[100][100], float bouncer_x, float bouncer_y) {
+	int randon_number = 0, i = 0, j = 0;
+	if ((*game_window) == 1) {
+		/*Далее,действие,при нажатии на кнопку Start:
+		Переход в режим многократных действий
+		*/
+		if ((bouncer_y > 0 && bouncer_y < 80) && (bouncer_x > 180 && bouncer_x < 330)) {
+			(*game_mode) = 10;
+		}
+		/*Далее,действие,при нажатии на кнопку Step:
+		Переход в режим однократного действия
+		*/
+		else if (((bouncer_y > 0 && bouncer_y < 80) && (bouncer_x > 340 && bouncer_x < 490))) {
+			(*game_mode) = 1;
+		}
+		/*Далее,действие,при нажатии на кнопку Stop:
+		Остановка игрового процесса
+		*/
+		else if ((bouncer_y > 0 && bouncer_y < 80) && (bouncer_x > 500 && bouncer_x < 670)) {
+			(*game_mode) = 3;
+		}
+		/*Далее,действие,при нажатии на кнопку Clear:
+		Обнуление матрицы b
+		*/
+		else if ((bouncer_y > 0 && bouncer_y < 80) && (bouncer_x > 680 && bouncer_x < 840)) {
+			clear(b);
+		}
+		/*Далее,действие,при нажатии на кнопку Menu:
+		Открытие стартового окна
+		*/
+		else if ((bouncer_y > 525 && bouncer_y < 600) && (bouncer_x > 800 && bouncer_x < 1000)) {
+			(*game_window) = 0;
+			(*count_of_clean) = 99;
+		}
+		/*Далее,деяствие,при нажатии на кнопку Planer :
+		Пtрвоначальная очиства матрицы b ,далее выполнение функции b1,которая присваивает матрице b нужные значения
+		*/
+		else if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 155 && bouncer_x < 260)) {
+			clear(b);
+			planer(b);
+		}
+		/*Далее,деяствие,при нажатии на кнопку Rellay :
+		Пtрвоначальная очиства матрицы b ,далее выполнение функции b2,которая присваивает матрице b нужные значения
+		*/
+		else if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 290 && bouncer_x < 380)) {
+			clear(b);
+			rellay(b);
+		}
+		//Кнопка "Twin Bees":
+		else if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 430 && bouncer_x < 570)) {
+			clear(b);
+			Twin_Bees(b);
+		}
+		/*Далее,деяствие,при нажатии на кнопку Random :
+		Каждой ячейче матрицы b присваивается рандомное значение 0 или 1
+		*/
+		else if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 10 && bouncer_x < 120)) {
+			srand((unsigned)time(NULL));
+			for (j = 1; j < size_of_matrix; j++) {
+
+				for (i = 1; i < size_of_matrix; i++) {
+
+					randon_number = rand() % 2;
+					b[i][j] = randon_number;
+				}
+			}
+		}
+		/*
+		Далее идет проверка попадания пользователем в игровые ячейки ,если координаты мыши удовлетворяют
+		определенным условиям(пользователь попадает в игровую ячейку,связанную с матрицей b) ТО:
+		если элемент b был равен 0 ,то он становится равным 1
+		если элемент b был равен 1 ,то он становится равным 0
+		*/
+		else {
+			int x, y;
+			for (y = 1; y < size_of_matrix; y++) {
+				if ((bouncer_y > 100 - 160 / size_of_matrix + (400 / size_of_matrix) * y) && (bouncer_y < 100 + 160 / size_of_matrix + (400 / size_of_matrix) * y))
+					for (x = 1; x < size_of_matrix; x++)
+					{
+						if ((bouncer_x > 320 - 160 / size_of_matrix + (400 / size_of_matrix) * x) && (bouncer_x < 320 + 160 / size_of_matrix + (400 / size_of_matrix) * x)) {
+							if (b[x][y] == 1) {
+								b[x][y] = 0;
+							}
+							else if ((b[x][y] == 0)) {
+								b[x][y] = 1;
+
+							}
+
+						}
+
+					}
+			}
+		}
+
+	}
+	if ((*game_window) == 0) {
+		if ((bouncer_y > 350 && bouncer_y < 390) && (bouncer_x > 410 && bouncer_x < 600)) {//Описание нажатия кнопки Играть
+			(*game_window) = 1;
+			(*size_settings )= 0;
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+			(*count_of_clean) = 99;
+			clear(b);
+
+		}
+		if ((bouncer_y > 450 && bouncer_y < 480) && (bouncer_x > 450 && bouncer_x < 550)) {//Описание нажатия кнопки Выход
+
+			(*Game_on) = 0;
+		}
+		
+	}
+}
+
 int planer(int b[100][100])
 {
 	b[2][1] = 1;
--- src/main.c
+++ src/main.c
@@ -5,28 +5,37 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_image.h>
 #include "allegro5/allegro_native_dialog.h"
-#include "functions.h"
 
-const float FPS = 60;
-const int SCREEN_W = 1000;
-const int SCREEN_H = 600;
+
+const float FPS = 60;//FPS
+const int SCREEN_W = 1000;//Ширина экрана
+const int SCREEN_H = 600;//Высота экрана 
 const int BOUNCER_SIZE = 32;
-int clear(int b[100][100]);
-int size_of_matrix = 40;
- float bouncer_x = 0.0;
- float bouncer_y = 0.0;
- int game_mode = 0;
- int planer(int b[100][100]);
- int rellay(int  b[100][100]);
- int Twin_Bees(int b[100][100]);
- int game_window = 0;
+int clear(int b[100][100]);//Очистка матрицы b
+int size_of_matrix = 40;//Переменная ,отвечающая за размер матрицы 
+ float bouncer_x = 0.0;//Координата нажатия  оси х
+ float bouncer_y = 0.0;//Координаты нажатия  оси у
+ /* Переменная game_mode отвечает за игровой режим ,если
+    game_mode==1 режим шага.
+	game_mode==10 режим многократных действий.
+	game_mode==3 режим остановки.
+ */
+ int game_mode = 0;    
  int count_of_clean = 99;
- void knopka(int size_of_matrixint,int *game_mode, int *game_window, int *count_of_clean, int b[100][100], float bouncer_x, float bouncer_y);
-
+ int planer(int b[100][100]);//Функции прорисовки различных фигур
+ int rellay(int  b[100][100]);//Функции прорисовки различных фигур
+ int Twin_Bees(int b[100][100]);//Функции прорисовки различных фигур
+ int game_window = 0;//Переменная game_window отвечает за открытие определенного окна ,если game_window=0 - стартовое ,game_window=1 -игровое
+ //Функция knopka обрабатывает координаты полученные при нажатии пользователем на экран в режиме игрового окна
+ void knopka(int size_of_matrix,int *Game_on, int *size_settings,int *game_mode, int *game_window, int *count_of_clean, int b[100][100], float bouncer_x, float bouncer_y);
+ //Функция size_matrix обрабатывает координаты полученные при нажатии пользователем на экран в режиме настрое размера поля 
+ void size_matrix(int *size_of_matrix, float bouncer_x, float bouncer_y);
+ //Функция main_algorithm отвечает за главный алгоритм игры (ищет соседей живых клеток ,далее выполняет действия в соответствии с правилами игры )
+ void main_algorithm(int b[100][100], int a[100][100],int number_of_neighbors,int size_of_matrix, int *game_mode);
 
 int main(int argc, char **argv)
 
-{
+{//С 38 по 119 строчку инициализация различных механизмов библиотеки 
 	ALLEGRO_DISPLAY_MODE *al_get_display_mode(int index, ALLEGRO_DISPLAY_MODE *mode);
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
@@ -71,27 +80,22 @@ int main(int argc, char **argv)
 		al_destroy_timer(timer);
 		return -1;
 	}
-	image = al_load_bitmap("startstepstopclean2.png");
-	image = al_load_bitmap("rules4.png");
-	image = al_load_bitmap("randomplanerrellay.png");
+	
 
-
-
+	image = al_load_bitmap("res/fon_menu.jpg");
 	if (!image) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
 			NULL, ALLEGRO_MESSAGEBOX_ERROR);
 		al_destroy_display(display);
 		return 0;
 	}
-
-	al_draw_bitmap(image, 200, 200, 50);
+	
+	al_draw_bitmap(image, 0, 0, 0);
 	al_set_target_bitmap(bouncer);
-
 	al_clear_to_color(al_map_rgb(255, 0, 255));
-
 	al_set_target_bitmap(al_get_backbuffer(display));
-
 	event_queue = al_create_event_queue();
+	
 	if (!event_queue) {
 		fprintf(stderr, "failed to create event_queue!\n");
 		al_destroy_bitmap(bouncer);
@@ -99,62 +103,39 @@ int main(int argc, char **argv)
 		al_destroy_timer(timer);
 		return -1;
 	}
-
 	al_install_keyboard();
 	al_register_event_source(event_queue, al_get_display_event_source(display));
-
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-
 	al_register_event_source(event_queue, al_get_mouse_event_source());
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
-
 	al_init_font_addon(); // initialize the font addon
 	al_init_ttf_addon();// initialize the ttf (True Type Font) addon
 
 	ALLEGRO_FONT *font_goldana = al_load_font("res/Goldana.ttf", 45, 0);
 	ALLEGRO_FONT *font_pirulen = al_load_font("res/pirulen.ttf", 24, 0);
-
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-
-
-
 	al_init_primitives_addon();
-
 	al_flip_display();
-
 	al_start_timer(timer);
-	int i, j, k, x, h, y = 1, n = 0, t = 0;
-	int a[100][100], b[100][100];
-	
-	
-	
-	int kol = 0, kol_max = 300, c1 = 0;
-	int life = 0, dead = 0;
-	clear(b);
-	
 
-
-
-
-
-	int g = 0;
-
+	int i=0, j=0, x=0, y = 0;//Переменные для циклов FOR
+	int randon_number = 0;   //Переменная отвечающая за рандомное распределение 
+	int a[100][100], b[100][100];//Две основные матрицы игры (их назначение описано в плане реализации проекта )
+	int number_of_neighbors = 0;//Счетчик количества соседей у каждой живой клетки 
+	int size_settings = 0; //Режим меню выбора размера матрицы(при size_settings=0 меню закрыто,при size_settings=1 меню открыто
+	int Game_on = 1;
 	al_clear_to_color(al_map_rgb(0, 0, 0));
+	clear(b);
 
-
-	while (1)
+	while (Game_on==1)
 	{
 		count_of_clean++;                         //Процесс очистки экрана,
 		if (count_of_clean == 100) {                      //при котором все ненужные элементы удаляются ,
 			count_of_clean = 0;                           // а нужные прорисовываются снова
-
-
-
 			if (game_window == 0) {                                  //Прорисовка стартового окна
 
 				image = al_load_bitmap("res/fon_menu.jpg"); //Загрузка фона для стартового окна
 				al_draw_bitmap(image, 0, 0, 0);             //Прорисовка фона для стартового окна
-				if (g == 1)
+				if (size_settings == 1)
 				{
 					image = al_load_bitmap("res/menu_op.jpg"); //Загрузка фона для стартового окна
 					al_draw_bitmap(image, 0, 0, 0);           //Прорисовка фона для стартового окна
@@ -199,80 +180,36 @@ int main(int argc, char **argv)
 
 		if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)   //Координаты обрабатываются при нажатии пользователем на экран
 		{
-			if (game_window == 1) {//Переменная game_window отвечает за открытие определенного окна ,если game_window=0 - стартовое ,game_window=1 -игровое
-
-
-				knopka(size_of_matrix, &game_mode, &game_window, &count_of_clean, b, bouncer_x, bouncer_y);
-				/*Далее,деяствие,при нажатии на кнопку Random :
-				Каждой ячейче матрицы b присваивается рандомное значение 0 или 1
-				*/
-				if ((bouncer_y > 500 && bouncer_y < 550) && (bouncer_x > 10 && bouncer_x < 120)) {
-					srand((unsigned)time(NULL));
-					for (j = 1; j < size_of_matrix; j++) {
-
-						for (i = 1; i < size_of_matrix; i++) {
-
-							t = rand() % 2;
-							b[i][j] = t;
-						}
-					}
-				}
-
-			}
+			knopka(size_of_matrix,&Game_on, &size_settings, &game_mode, &game_window, &count_of_clean, b, bouncer_x, bouncer_y);
+			
 			/*
-			Описание обработки результатов в режиме стартового окна  */
+			Прорисовка меню настроек*/
 			if (game_window == 0) {
-				if ((bouncer_y > 350 && bouncer_y < 390) && (bouncer_x > 410 && bouncer_x < 600)) {//Описание нажатия кнопки Играть
-					game_window = 1;
-					g = 0;
-					al_clear_to_color(al_map_rgb(0, 0, 0));
-					count_of_clean = 99;
-					clear(b);
-
-				}
-				if ((bouncer_y > 450 && bouncer_y < 480) && (bouncer_x > 450 && bouncer_x < 550)) {//Описание нажатия кнопки Выход
-
-					break;
-				}
+				
 				if ((bouncer_y > 390 && bouncer_y < 430) && (bouncer_x > 420 && bouncer_x < 600)) {//Описание нажатия кнопки Настройки
 
 					al_clear_to_color(al_map_rgb(0, 0, 0));
-					if (g == 0) {
-						g = 1;
+					if (size_settings == 0) {
+						size_settings = 1;
 
 						image = al_load_bitmap("res/menu_op.jpg");
 						al_draw_bitmap(image, 0, 0, 0);
 					}
-					else if (g == 1) {
-						g = 0;
+					else if (size_settings == 1) {
+						size_settings = 0;
 						image = al_load_bitmap("res/fon_menu.jpg");
 						al_draw_bitmap(image, 0, 0, 0);
 					}
 				}
 			}
 			/*
-			Действия ,при выборе размера поля (Переменная m отвечает за размер поля  */
-			if (g == 1) {
-				if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && (bouncer_y > 370 && bouncer_y < 405) && (bouncer_x > 640 && bouncer_x < 850)) {
-					size_of_matrix = 10;
-
-
-				}
-				else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && (bouncer_y > 405 && bouncer_y < 435) && (bouncer_x > 640 && bouncer_x < 850)) {
-					size_of_matrix = 20;
-
-				}
-				else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && (bouncer_y > 435 && bouncer_y < 465) && (bouncer_x > 640 && bouncer_x < 850)) {
-					size_of_matrix = 30;
-
-				}
-				else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && (bouncer_y > 465 && bouncer_y < 495) && (bouncer_x > 640 && bouncer_x < 850)) {
-					size_of_matrix = 40;
-
-				}
+			Функция выбора размера поля */
+		if (size_settings == 1) {
+			size_matrix(&size_of_matrix, bouncer_x, bouncer_y);
 			}
 		}
 		if (game_window == 1) {
+			//Прорисовка игровых ячеек 
 			for (y = 1; y < size_of_matrix; y++) {
 
 				for (x = 1; x < size_of_matrix; x++)
@@ -287,7 +224,7 @@ int main(int argc, char **argv)
 				}
 			}
 
-
+			//Остановка игрового цикла 
 			if (game_mode == 3)
 			{
 				game_mode = 0;
@@ -297,9 +234,8 @@ int main(int argc, char **argv)
 			else if (game_mode == 10 || game_mode == 1)
 			{
 				if (game_mode == 1) game_mode = 0;
-
+				//Прорисовка игровых ячеек 
 				for (y = 1; y < size_of_matrix; y++) {
-
 					for (x = 1; x < size_of_matrix; x++)
 					{
 						if (b[x][y] == 1) {
@@ -309,73 +245,29 @@ int main(int argc, char **argv)
 						else if (b[x][y] == 0) {
 							al_draw_filled_ellipse(320 + (400 / size_of_matrix) * x, 100 + (400 / size_of_matrix) * y, 160 / size_of_matrix, 160 / size_of_matrix, al_map_rgb(255, 255, 255));
 						}
-
 					}
 				}
-				for (j = 1; j < size_of_matrix; j++) {
-					for (i = 1; i <size_of_matrix; i++) {
-						if (b[i - 1][j - 1] == 1) k++;
-						if (b[i][j - 1] == 1) k++;
-						if (b[i + 1][j - 1] == 1) k++;
-						if (b[i - 1][j] == 1) k++;
-						if (b[i + 1][j] == 1) k++;
-						if (b[i - 1][j + 1] == 1) k++;
-						if (b[i][j + 1] == 1) k++;
-						if (b[i + 1][j + 1] == 1) k++;
-						if (b[i][j] == 1)
-						{
-							if ((k == 2) || (k == 3))a[i][j] = 1;
-							else {
-								a[i][j] = 0;
-								dead++;
-							}
-						}
-						if (b[i][j] == 0) {
-							if ((k == 3)) {
-								a[i][j] = 1;
-								life++;
-							}
-							else a[i][j] = 0;
-						}
-						k = 0;
-
-					}
-				}
-				int d = 0;
-				for (y = 1; y < size_of_matrix; y++) {
-					for (x = 1; x < size_of_matrix; x++)
-					{
-						if (b[x][y] == a[x][y]) d++;
-
-					}
-				}
-				if (d >size_of_matrix*size_of_matrix - 2 * size_of_matrix) { d = 0; game_mode = 0; }
-
-
-				for (y = 1; y < size_of_matrix; y++) {
-					for (x = 1; x < size_of_matrix; x++)
-					{
-						b[x][y] = a[x][y];
-						redraw = true;
-					}
-				}
+				//Выполнение главного алгоритма игры 
+				main_algorithm( b,  a,  number_of_neighbors,  size_of_matrix,&game_mode);
+				
 
 			}
 
 		}
-
+		//Следующая часть кода отвечает за красные и зеленые ячейки напротив размера поля 
 		if (game_window == 0) {
-			if (g == 1) {
+			if (size_settings == 1) {
 				al_draw_filled_ellipse(650, 390, 5, 5, al_map_rgb(255, 0, 2));
 				al_draw_filled_ellipse(650, 420, 5, 5, al_map_rgb(255, 0, 2));
 				al_draw_filled_ellipse(650, 450, 5, 5, al_map_rgb(255, 0, 2));
 				al_draw_filled_ellipse(650, 480, 5, 5, al_map_rgb(255, 0, 2));
+
+				if (size_of_matrix == 40  ) al_draw_filled_ellipse(650, 480, 5, 5, al_map_rgb(0, 255, 2));
+				if (size_of_matrix == 30  ) al_draw_filled_ellipse(650, 450, 5, 5, al_map_rgb(0, 255, 2));
+				if (size_of_matrix == 20  ) al_draw_filled_ellipse(650, 420, 5, 5, al_map_rgb(0, 255, 2));
+				if (size_of_matrix == 10  ) al_draw_filled_ellipse(650, 390, 5, 5, al_map_rgb(0, 255, 2));
+				al_flip_display();
 			}
-			if (size_of_matrix == 40 && g == 1) al_draw_filled_ellipse(650, 480, 5, 5, al_map_rgb(0, 255, 2));
-			if (size_of_matrix == 30 && g == 1) al_draw_filled_ellipse(650, 450, 5, 5, al_map_rgb(0, 255, 2));
-			if (size_of_matrix == 20 && g == 1) al_draw_filled_ellipse(650, 420, 5, 5, al_map_rgb(0, 255, 2));
-			if (size_of_matrix == 10 && g == 1) al_draw_filled_ellipse(650, 390, 5, 5, al_map_rgb(0, 255, 2));
-			al_flip_display();
 		}
 		al_flip_display();
 
