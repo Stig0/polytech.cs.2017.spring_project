130f582bb8dc58224d37d5e3e11a3b466ad41955
Stig0 <danilapuzko@mail.ru>
2017-05-06 20:24:53 +0300

    add functions.h

---
+++ src/functions.h
@@ -0,0 +1,5 @@
+void main_algorithm(int b[100][100], int a[100][100], int number_of_neighbors, int size_of_matrix,int *game_mode);
+int clear(int b[100][100]);
+int planer(int b[100][100]);
+int rellay(int b[100][100]);
+int Twin_Bees(int b[100][100]);
