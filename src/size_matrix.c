223c3fd38dd0f40c9022eee60c3404a8d1f37497
Stig0 <danilapuzko@mail.ru>
2017-05-25 19:51:35 +0300

    add assert to size_matrix

--- src/size_matrix.c
+++ src/size_matrix.c
@@ -5,6 +5,8 @@
     *@author Nikolaev Denis - den11nik@yandex.ru \n
     *@author Puzko Danila - danilapuzko@mail.ru*/
 #include "size_matrix.h"
+#include "assert.h"
+#define NULL 0
 /**  \fn void size_matrix(int *size_of_matrix, float bouncer_x, float bouncer_y)
         *Функция анализирует координаты нажатия мышью в режиме открытых настроек ,и изменяет размер матрицы
     \param int *size_of_matrix -указатель на размер матрицы
@@ -13,6 +15,7 @@
     \return это void-функция
 */
 void size_matrix(int *size_of_matrix, float bouncer_x, float bouncer_y) {
+	assert(size_of_matrix!=NULL);
 	if ( (bouncer_y > 370 && bouncer_y < 405) && (bouncer_x > 640 && bouncer_x < 850)) {
 		(*size_of_matrix )= 11;
 
